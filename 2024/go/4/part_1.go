7813582db55c8c8486b68d184477b6a0d30d3bfa
ktran1005 <kdt57@drexel.edu>
2024-12-07 09:39:00 -0500

    working on day 4

---
+++ 2024/go/4/part_1.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+
+func inside(row int, col int, height int, width int) bool {
+	return 0 <= row && row < height && 0 <= col && col < width 
+}
+
+func main() {
+	// Open the file
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		fmt.Println("Error opening file: ", err)
+		return
+	}
+	defer file.Close()
+
+	var s []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		s = append(s, line)
+	}
+
+	height := len(s)
+	width := len(s[0])
+	string_look_for := "XMAS"
+	answer := 0
+
+	for row := 0; row < height; row++ {
+		for col := 0; col < width; col++ {
+			if string(s[row][col]) == "X" {
+				for drow := -1; drow <= 1; drow++ {
+					for dcol := -1; dcol <= 1; dcol++ {
+						if drow == 0 && dcol == 0 {
+							continue
+						}
+						all_ok := true
+						for i:= 0; i < 4; i++ {    // row = 0, col = 5
+							r2 := row + drow * i   // row = 0 ; drow = 0; i = 1 => r2 = 0 
+							c2 := col + dcol * i   // col = 5; dcol = 1; i = 1 => c2 = 4
+							if inside(r2, c2, height, width) && string(s[r2][c2]) == string(string_look_for[i]) {
+							} else {
+								all_ok = false
+								break
+							}
+						}
+						if all_ok {
+							answer += 1
+						}
+
+					}
+				}
+			} 
+		}
+	}
+	fmt.Println(answer)
+}
