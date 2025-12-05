9e10300ab3132509a930f888c2a0ccf9add49dcb
ktran1005 <kdt57@drexel.edu>
2025-12-05 16:06:07 -0500

    added solution for part 2 day 5

--- 2025/python/5/solution.py
+++ 2025/python/5/solution.py
@@ -3,7 +3,6 @@ def solution_part_1(input):
         rangeArr = []
         count = 0
         data = file.read().strip().splitlines()
-        # print(data)
         for i in range(len(data)):
             if '-' in data[i]:
                 data[i] = data[i].split('-')
@@ -19,7 +18,36 @@ def solution_part_1(input):
                         break
         print(count)
 
+def solution_part_2(input):
+    # Technique: Merge Intervals
+    with open(input, 'r') as file:
+        interval = []
+        data = file.read().strip().splitlines()
+        res = []
+        count = 0
+        for i in range(len(data)):
+            if '-' in data[i]:
+                data[i] = data[i].split('-')
+                interval.append([int(data[i][0]), int(data[i][1])])
+            else:
+                break
+    
+        interval.sort()
+
+        for i in range(len(interval)):
+            if not res:
+                res.append(interval[i])
+            else:
+                if interval[i][0] <= res[-1][1]:
+                    res[-1][1] = max(res[-1][1], interval[i][1])
+                else:
+                    res.append(interval[i])
+        
+        for i in range(len(res)):
+            count += res[i][1] - res[i][0] + 1
+        print(count)
 
 
 if __name__ == "__main__":
     solution_part_1('input.txt')
+    solution_part_2('input.txt')
