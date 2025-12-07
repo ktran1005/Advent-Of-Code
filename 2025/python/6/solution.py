4051ed2421a89bb0be19db5c4bd6f49fdc2f1f3c
ktran1005 <kdt57@drexel.edu>
2025-12-06 19:47:40 -0500

    added solution for part 2 day 6

--- 2025/python/6/solution.py
+++ 2025/python/6/solution.py
@@ -33,41 +33,43 @@ def solution_part_1(input):
 
 def solution_part_2(input):
     with open(input, "r") as file:
+        data = file.read().split('\n')
+        ops = data[-1]
+        number_data = data[:-1]
+        
+        line_start = 0
         total = 0
-        data = file.read().strip().splitlines()
-        for i in range(len(data)):
-            data[i] = (" ".join(data[i].split())).split(" ")
-        
-        num_rows = len(data)
-        num_cols = len(data[0])
 
-        transposed = []
-        hashMap = {}
-        for i in range(num_cols):
-            curCols = []
-            maxLen = 0
-            for j in range(num_rows):
-                curCols.append(data[j][i])
-                maxLen = max(maxLen, len(data[j][i]))
-            transposed.append(curCols)
-            hashMap[i] = maxLen
-        
-        print(hashMap)
-        print(transposed)
-        for i in range(len(transposed) - 1, -1, -1):
-            temp = ""
-            k = 0
-            while k < hashMap[i]:
-                for j in range(len(transposed[i]) - 1):
-                    if len(transposed[i][j]) < hashMap[i] and k < hashMap[i] - len(transposed[i][j]):
-                        continue
-                # while k < hashMap[i]:
-                #     if len(transposed[i][j]) < hashMap[i] and k < hashMap[i] - len(transposed[i][j]):
-                #         continue
-                #     temp += transposed[i][j][k]
-                #     k += 1
+        while line_start < len(ops):
+            current_ops = ops[line_start]
+            line_ends = line_start + 1
+            while line_ends < len(ops) and ops[line_ends] == " ":
+                line_ends += 1
+            
+            if line_ends != len(ops):
+                line_ends -= 2
+            else:
+                line_ends -= 1
+            
+            val = 0 if current_ops == "+" else 1
+            
+            for i in range(line_start, line_ends + 1):
+                current_number = 0
+                for line in number_data:
+                    if line[i] != " ":
+                        current_number *= 10
+                        current_number += int(line[i])
                 
+                if current_ops == "+":
+                    val += current_number
+                else:
+                    val *= current_number
+
+            line_start = line_ends + 2
+            total += val
+
+        print(total)
 
 if __name__ == "__main__":
-    # solution_part_1("input.txt")
+    solution_part_1("input.txt")
     solution_part_2("input.txt")
