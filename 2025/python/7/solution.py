cb9685f171686d8082883b704dd820b78633e3e5
ktran1005 <kdt57@drexel.edu>
2025-12-07 14:36:20 -0500

    finished part 1 day 7

--- 2025/python/7/solution.py
+++ 2025/python/7/solution.py
@@ -1,36 +1,28 @@
 from collections import defaultdict
 def solution_part_1(input):
     with open(input, 'r') as file:
-        count = 0
-        start = None
-        first_stop = True
         data = file.read().strip().splitlines()
-        track = []
+        count = 0
+        current = set()
+        i = 0
         for row in range(len(data)):
-            hashMap = defaultdict(list)
-            for col in range(len(data[0])):
+            for col in range(len(data[row])):
                 if data[row][col] == "S":
-                    start = (row, col)
+                    current.add((row, col))
+        print(current)
+        while len(current) > 0:
+            next = set()
+            for x, y in current:
+                if x + 1 >= len(data):
                     continue
-                if data[row][col] == "^" and first_stop: 
-                    first_stop = False
-                    track.append(col)
-                    continue
-                
-                if track:
-                    if data[row][col] == "^":
-                        if col not in track:
-                            # print("row {} col {} plus 1".format(row, col))
-                            count += 1
-                            hashMap[row].append(col)
+                if data[x][y] == "^":
+                    next.add((x + 1, y - 1))
+                    next.add((x + 1, y + 1))
+                    count += 1
+                else:
+                    next.add((x + 1, y))
             
-            if len(hashMap[row]):
-                while track:
-                    track.pop()
-                for c in hashMap[row]:
-                    track.append(c)
-                print("track: {} after adding new element at row {}; count {}".format(track, row, count))
-
+            current = list(set(next))
         print(count)
 if __name__ == "__main__":
     solution_part_1('input.txt')
