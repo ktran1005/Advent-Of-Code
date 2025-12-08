c291f9b3a3d6c7de1bb4b4ea538ce29bed515bd8
ktran1005 <kdt57@drexel.edu>
2025-12-08 17:10:46 -0500

    investigate performance issue

--- 2025/python/7/solution.py
+++ 2025/python/7/solution.py
@@ -1,15 +1,14 @@
-from collections import defaultdict
+from collections import Counter
 def solution_part_1(input):
     with open(input, 'r') as file:
         data = file.read().strip().splitlines()
         count = 0
         current = set()
-        i = 0
         for row in range(len(data)):
             for col in range(len(data[row])):
                 if data[row][col] == "S":
                     current.add((row, col))
-        print(current)
+
         while len(current) > 0:
             next = set()
             for x, y in current:
@@ -22,7 +21,42 @@ def solution_part_1(input):
                 else:
                     next.add((x + 1, y))
             
-            current = list(set(next))
+            current = next
         print(count)
+
+def solution_part_2(input):
+    with open(input, 'r') as file:
+        data = file.read().strip().splitlines()
+        count = 0
+        current = []
+        for row in range(len(data)):
+            for col in range(len(data[row])):
+                if data[row][col] == "S":
+                    current.append((row, col, 1))
+
+        while len(current) > 0:
+            next = []
+            for x, y, c in current:
+                if x + 1 >= len(data):
+                    count += c
+                    continue
+                if data[x][y] == "^":
+                    next.append((x + 1, y - 1, c))
+                    next.append((x + 1, y + 1, c))
+                else:
+                    next.append((x + 1, y, c))
+            
+            p = Counter()
+            for (x, y, c) in next:
+                p[(x, y)] += c 
+
+
+            for x, y in p.keys():
+                current.append((x, y, p[(x, y)]))
+        print(count)
+
+
+
 if __name__ == "__main__":
-    solution_part_1('input.txt')
+    # solution_part_1('input.txt')
+    solution_part_2("input.txt")
